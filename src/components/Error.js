30f87233ce7cab97f8124a58ec1625309c9b7bc5
Therese Lindén <theresemlinden@gmail.com>
2021-02-02 18:13:55 +0100

    styled error page, added content-security-policy

--- src/components/Error.js
+++ src/components/Error.js
@@ -11,10 +11,15 @@ const Error = () => {
   };
 
   return (
-    <>
-      <p>Something went wrong, try again later.</p>
-      <Button type="button" text="Go back home" onClick={handleGoBack} />
-    </>
+    <section className="error">
+      <h2 className="error-heading">Something went wrong, try again later.</h2>
+      <Button
+        type="button"
+        text="Go back home"
+        onClick={handleGoBack}
+        className="category-button"
+      />
+    </section>
   );
 };
 export default Error;
